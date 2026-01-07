37f6add11efc416fdc7a15b5b52803badbcb3f4e
Einfachirgendwa1 <153438063+Einfachirgendwa1@users.noreply.github.com>
2026-01-07 18:52:36 +0100

    initial commit (rust btw rust rust)

---
+++ src/tests.rs
@@ -0,0 +1,52 @@
+﻿#[cfg(test)]
+mod tests {
+    use crate::{DataReceiverExt, PackageV1, SpoofedData, END, MAGIC};
+
+    #[test]
+    fn basic() {
+        let mut spoofed = SpoofedData::from(&[
+            &3i32.to_le_bytes(),
+            &394i32.to_le_bytes(),
+            &MAGIC as &[u8],
+            &1i32.to_le_bytes(),
+            &8i32.to_le_bytes(),
+            &42i32.to_le_bytes(),
+            &7i32.to_le_bytes(),
+            &END,
+            &394i32.to_le_bytes(),
+            &3i32.to_le_bytes(),
+        ]);
+
+        let package = spoofed.read_next_package().unwrap();
+        let package_v1 = PackageV1::try_from(package).unwrap();
+
+        assert_eq!(package_v1.timestamp, 42);
+        assert_eq!(package_v1.value, 7);
+    }
+
+    #[test]
+    fn no_magic() {
+        let mut spoofed = SpoofedData::from(&[
+            &1i32.to_le_bytes(),
+            &8i32.to_le_bytes(),
+            &42i32.to_le_bytes(),
+            &7i32.to_le_bytes(),
+            &END as &[u8],
+        ]);
+
+        assert_eq!(spoofed.read_next_package(), None);
+    }
+
+    #[test]
+    fn no_end() {
+        let mut spoofed = SpoofedData::from(&[
+            &MAGIC as &[u8],
+            &1i32.to_le_bytes(),
+            &8i32.to_le_bytes(),
+            &42i32.to_le_bytes(),
+            &7i32.to_le_bytes(),
+        ]);
+
+        assert_eq!(spoofed.read_next_package(), None);
+    }
+}
